cd510141d1172590e1838cc2c856183edcab6f77
Alex-Filini <Filini97@yandex.ru>
2023-10-09 14:22:54 +0300

    Server fixing in progress.

--- 01_web/http-server/src/main/java/ru/netology/Server.java
+++ 01_web/http-server/src/main/java/ru/netology/Server.java
@@ -25,74 +25,76 @@ public class Server {
     }
 
     public void start() {
-        try (var serverSocket = new ServerSocket(this.port)) {
-            while (!serverSocket.isClosed()) {
-                try (var socket = serverSocket.accept()) {
-                    executorService.execute(() -> {
-                        try {
-                            connect(socket);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var socket = serverSocket.accept();
+                    executorService.submit(() -> {
+                        connect(socket);
                     });
                 }
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void connect(Socket socket) throws IOException {
+    public void connect(Socket socket) {
+        try (socket;
         final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        final var out = new BufferedOutputStream(socket.getOutputStream());
+        final var out = new BufferedOutputStream(socket.getOutputStream());) {
 
-        final var requestLine = in.readLine();
-        final var parts = requestLine.split(" ");
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
 
-        if (parts.length != 3) {
-            socket.close();
-        }
+            if (parts.length != 3) {
+                socket.close();
+                return;
+            }
 
-        final var path = parts[1];
-        if (!validPaths.contains(path)) {
-            out.write((
-                    "HTTP/1.1 404 Not Found\r\n" +
-                            "Content-Length: 0\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            out.flush();
-        }
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
 
-        final var filePath = Path.of(".", "public", path);
-        final var mimeType = Files.probeContentType(filePath);
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
 
-        if (path.equals("/classic.html")) {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                return;
+            }
+
+            final var length = Files.size(filePath);
             out.write((
                     "HTTP/1.1 200 OK\r\n" +
                             "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + content.length + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
                             "Connection: close\r\n" +
                             "\r\n"
             ).getBytes());
-            out.write(content);
+            Files.copy(filePath, out);
             out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        final var length = Files.size(filePath);
-        out.write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        Files.copy(filePath, out);
-        out.flush();
     }
 }
