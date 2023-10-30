5d737c14b655381f870c6a3de37e827162d9e3c8
Alex-Filini <Filini97@yandex.ru>
2023-10-30 18:14:34 +0300

    MainServlet fixed.

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -12,6 +12,13 @@ import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
   private PostController controller;
+  private static final String GET = "GET";
+  private static final String POST = "POST";
+  private static final String DELETE = "DELETE";
+  private static final String API_POSTS = "/api/posts";
+  private static final int NOT_FOUND_STATUS = HttpServletResponse.SC_NOT_FOUND;
+  private static final int INTERNAL_SERVER_ERROR_STATUS = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+
 
   @Override
   public void init() {
@@ -26,31 +33,31 @@ public class MainServlet extends HttpServlet {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
       // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
+      if (method.equals(GET) && path.equals(API_POSTS)) {
         controller.all(resp);
         return;
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+      if (method.equals(GET) && path.matches(API_POSTS + "/\\d+")) {
         // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
         controller.getById(id, resp);
         return;
       }
-      if (method.equals("POST") && path.equals("/api/posts")) {
+      if (method.equals(POST) && path.equals(API_POSTS)) {
         controller.save(req.getReader(), resp);
       }
 
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+      if (method.equals(DELETE) && path.matches(API_POSTS + "/\\d+")) {
         // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
         controller.removeById(id, resp);
       }
     } catch (NotFoundException e) {
       e.getMessage();
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      resp.setStatus(NOT_FOUND_STATUS);
     } catch (IOException ioException) {
       ioException.getMessage();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      resp.setStatus(INTERNAL_SERVER_ERROR_STATUS);
     }
   }
 }
