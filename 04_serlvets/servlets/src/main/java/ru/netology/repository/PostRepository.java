88ac6253c0bc1a8932a48235d220cd938d8be90b
Alex-Filini <Filini97@yandex.ru>
2023-11-01 13:34:18 +0300

    Di-java implemented.

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,54 +1,16 @@
 package ru.netology.repository;
 
-import org.springframework.stereotype.Repository;
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
-@Repository
-public class PostRepository {
-  private final ConcurrentHashMap<Long, Post> posts;
-  private final AtomicLong idCounter = new AtomicLong(0);
+public interface PostRepository {
+  List<Post> all();
 
-  public PostRepository() {
-    this.posts = new ConcurrentHashMap<>();
-  }
+  Optional<Post> getById(long id);
 
-  public List<Post> all() {
-    return new ArrayList<>(posts.values());
-  }
+  Post save(Post post);
 
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
-
-  public Post save(Post post) {
-    if (post.getId() != 0) {
-      if (posts.containsKey(post.getId())) {
-        posts.put(post.getId(), post);
-        return post;
-      } else {
-        throw new NotFoundException();
-      }
-    } else {
-      long newId = idCounter.incrementAndGet();
-      post.setId(newId);
-      posts.put(newId, post);
-      return post;
-    }
-  }
-
-  public void removeById(long id) {
-    if (posts.containsKey(id)) {
-      posts.remove(id);
-    }
-    else {
-      throw new NotFoundException("wrong id");
-    }
-  }
+  void removeById(long id);
 }
